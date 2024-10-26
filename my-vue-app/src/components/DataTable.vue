885b1a527595518563fdd3f325cad63863724a08
gremvanek <gremvanek@gmail.com>
2024-10-26 11:43:33 +0500

    Front CRUD Item

--- my-vue-app/src/components/DataTable.vue
+++ my-vue-app/src/components/DataTable.vue
@@ -14,7 +14,7 @@
         </template>
         <template #item.actions="{ item }">
             <v-icon small @click="edititem(item)">mdi-pencil</v-icon>
-            <v-icon small @click="deleteitem(item)">mdi-delete/v-icon>
+            <v-icon small @click="deleteitem(item)">mdi-delete</v-icon>
         </template>
     </v-data-table>
 
@@ -25,3 +25,41 @@
 <script>
 import { ref, onMounted } from 'vue';
 import { useDataStore } from '../stores/dataStore.js';
+
+export default{
+    setup(){
+        const store = useDataStore();
+        const headers = ref([
+            {text: 'ID', value:'id'},
+            {text: 'Название', value: 'name'},
+            {text: 'Действия', value: 'actions', sortable: false}
+        ]);
+
+        const loading = ref(true);
+        const items = store.items;
+
+        onMounted(async () => {
+            await store.loaditems();
+            loading.value = false;
+        });
+
+        const additem = () => {
+            store.additem({id: Date.now(), name:'Новый элемент'});            
+        };
+
+        const edititem = (item) => {
+            const Newname = prompt('Смена имени item', item.name);
+            if(Newname !== null){
+                store.updateitem({...item, name: Newname });
+            }
+        };
+
+        const deleteitem = (item) =>{
+            if(confirm('Вы точно хотите удалить item?')){
+                store.deleteitem(item.id);
+            }
+        };
+    return { headers, items, additem, edititem, deleteitem, loading };
+    }
+}
+</script>
