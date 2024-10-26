1d88b8866f2b748a3524b173350ac8771f8d9896
gremvanek <gremvanek@gmail.com>
2024-10-26 11:16:06 +0500

    Конфиг main

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify.js';
+import { createPinia } from 'pinia';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(vuetify);
+app.use(createPinia());
+app.mount('#app');
