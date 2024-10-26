8afd0f9bad72b0f3c86e088fe81729e771d830a6
gremvanek <gremvanek@gmail.com>
2024-10-26 11:27:33 +0500

    Написание DataTable фронт для Item

---
+++ my-vue-app/src/components/DataTable.vue
@@ -0,0 +1,27 @@
+<template>
+<v-container>
+    <v-data-table
+        :headers="headers"
+        :items="items"
+        item-value="id"
+        class="elevation-1"
+        >
+        <template #top>
+        <v-toolbar flat>
+            <v-spacer></v-spacer>
+            <v-btn @click="additem">Добавить элемент</v-btn>
+        </v-toolbar>
+        </template>
+        <template #item.actions="{ item }">
+            <v-icon small @click="edititem(item)">mdi-pencil</v-icon>
+            <v-icon small @click="deleteitem(item)">mdi-delete/v-icon>
+        </template>
+    </v-data-table>
+
+<v-progress-linear v-if="loading" indeterminate></v-progress-linear>
+</v-container>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import { useDataStore } from '../stores/dataStore.js';
